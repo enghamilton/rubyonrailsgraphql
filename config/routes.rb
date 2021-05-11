ed0c8830fb19e4c2e1693fa8e7be1340f18fe6d3
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2021-05-10 22:40:38 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/gq", graphql_path: "/graphql" if Rails.env.development?
+  #mount GraphiQL::Rails::Engine, at: "/gq", graphql_path: "/graphql" if Rails.env.development?
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
   post "/graphql", to: "graphql#execute"
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
